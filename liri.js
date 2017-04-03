1e1807256950a8cadf2becc1d15b0d29daf6395b
emkamp <emily.pulskamp@gmail.com>
2017-04-02 22:01:17 -0500

    DONE: random.txt.  All required features completed.

--- liri.js
+++ liri.js
@@ -2,26 +2,15 @@ var Twitter = require("twitter");
 var Spotify = require("spotify");
 var Omdb = require("omdb");
 var Request = require("request");
-
-var userCommand = process.argv[2];
+var fs = require("fs");
 var keys = require('./keys');
 
-/* Make it so liri.js can take in one of the following commands:
-   my-tweets
-   spotify-this-song
-   movie-this
-   do-what-it-says             
-*/
+var userCommand = process.argv[2];
 
 console.log("============================= USER COMMAND ========");
 console.log(userCommand);
 
-// node liri.js my-tweets
-if (userCommand === "my-tweets") {
-    var client = new Twitter(keys.twitterKeys);
-    var tweetCount = 20;
-    var params = { screen_name: 'emkamp_atx', count: tweetCount };
-
+function getTweets() {
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
             console.log("============================ " + tweetCount + " RECENT TWEETS =====");
@@ -51,18 +40,13 @@ if (userCommand === "my-tweets") {
                 console.log(tweetFaves + " Favorites  |  " + tweetRetweets + " Retweets");
                 console.log("---------------------------------------------------");
             }
-
         } else if (error) {
             console.log(error);
         }
     });
+}
 
-    // node liri.js spotify-this-song song-title
-} else if (userCommand === "spotify-this-song") {
-    console.log("========================= SPOTIFY TOP RESULT ======");
-
-    var songTitle = process.argv[3];
-
+function getSongData(songTitle) {
     if (songTitle === undefined) {
         console.log("You didn't enter a song title, so I'll pick one for you.");
         Spotify.search({ type: 'track', query: 'the sign' }, function (err, data) {
@@ -70,7 +54,6 @@ if (userCommand === "my-tweets") {
                 console.log('Error occurred: ' + err);
                 return;
             }
-
             var aobResult = data.tracks.items[2];
             console.log('"' + aobResult.name + '" is by ' + aobResult.artists[0].name + ', from the album "' + aobResult.album.name + '"');
             console.log("Preview the track here: " + aobResult.preview_url);
@@ -90,32 +73,12 @@ if (userCommand === "my-tweets") {
             }
         });
     }
-} else if (userCommand === "movie-this") {
-    var movieTitle = process.argv[3];
-    var movieTitleSpaced = movieTitle.replace(/-/g, ' ').toLowerCase();
+}
 
-    // PUT GETMOVIEDEFAULT HERE
-
-    console.log("============================ OMDB TOP RESULT ======");
-    /*
-       ** Title of the movie.
-       ** Year the movie came out.
-       ** IMDB Rating of the movie.
-       ** Country where the movie was produced.
-       ** Language of the movie.
-       ** Plot of the movie.
-       ** Actors in the movie.
-       ** Rotten Tomatoes Rating.
-       * Rotten Tomatoes URL.
-     */
-
-    Omdb.search(movieTitleSpaced, function (err, movies) {
+function getMovieData(title) {
+    Omdb.search(title, function (err, movies) {
         if (err) {
             return console.error(err);
-        }
-        if (movieTitleSpaced === undefined) {
-            console.log("You didn't enter a movie title, so I picked one for you.");
-            getMovieDefault();
         }
 
         var topMovie = movies[0];
@@ -124,35 +87,71 @@ if (userCommand === "my-tweets") {
                 return console.error(err);
             } else if (!movie) {
                 console.log("Movie not found! I'll pick one for you instead");
-                getMovieDefault();
+                getMovieData(movieTitle);
             } else {
-                //console.log(movie);
                 var movieActors = movie.actors.toString().replace(/,/g, ", ");
+                var movieCountries = movie.countries.toString().replace(/,/g, ", ");
 
                 // many movies don't have a country listed
-                if (movie.countries === undefined){
+                if (movie.countries === undefined) {
                     var movieProduction = "OMDB doesn't know where it was produced.";
                 } else {
-                    var movieProduction = "It was produced in " + movie.countries;
+                    var movieProduction = "It was produced in " + movieCountries;
                 }
 
                 // a lot of movies don't have rotten tomatoes ratings
-                if (movie.tomato === undefined){
+                if (movie.tomato === undefined) {
                     var movieRatings = "Ratings: IMDB, " + movie.imdb.rating + " // Rotten Tomatoes has no rating for it.";
                 } else {
                     var movieRatings = "Ratings: IMDB, " + movie.imdb.rating + " // Rotten Tomatoes, " + movie.tomato;
                 }
 
+                // Output
                 console.log('"' + movie.title + '" was released in ' + movie.year + '. ' + movieProduction);
                 console.log(movieRatings);
                 console.log("Starring: " + movieActors);
                 // none of the movies I searched for had a language entry in the response object
-                console.log("I'd tell you what language(s) it was released in, but OMDB doesn't seem to have that information."); 
+                console.log("I'd tell you what language(s) it was released in, but OMDB doesn't seem to have that information.");
                 console.log("-- Plot --")
                 console.log(movie.plot);
             }
             console.log('---------------------------------------------------');
         });
+    });
+}
+
+// node liri.js my-tweets
+if (userCommand === "my-tweets") {
+    var client = new Twitter(keys.twitterKeys);
+    var tweetCount = 20;
+    var params = { screen_name: 'emkamp_atx', count: tweetCount };
+
+    // node liri.js spotify-this-song song-title
+} else if (userCommand === "spotify-this-song") {
+    console.log("========================= SPOTIFY TOP RESULT ======");
+    var songTitleInput = process.argv[3];
+    getSongData(songTitleInput);
+
+    // node liri.js movie-this movie-title
+} else if (userCommand === "movie-this") {
+    var movieTitle = process.argv[3];
+    console.log("============================ OMDB TOP RESULT ======");
+
+    if (movieTitle === undefined) {
+        console.log("You didn't enter a movie title, so I picked one for you.");
+        console.log('---------------------------------------------------');
+        getMovieData("Mr. Nobody");
+    } else {
+        var movieTitleSpaced = movieTitle.replace(/-/g, ' ').toLowerCase();
+        getMovieData(movieTitleSpaced);
+    }
+
+    // node liri.js do-what-it-says
+} else if (userCommand === "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function read(err, data) {
+        var notSoRandom = data.split(' ');
+        console.log("======================= WHAT'S IN THIS FILE? ======");
+        getSongData(notSoRandom[1]);
     });
 } else {
     console.log("please enter a valid command");
